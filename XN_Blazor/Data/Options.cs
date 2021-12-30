56b8edb87837c968e551ed1e8c436d96e297a700
Gwonhee <gwon_hee@naver.com>
2021-12-30 16:56:53 +0900

    Reset..Date..Fxxk

--- XN_Blazor/Data/Options.cs
+++ XN_Blazor/Data/Options.cs
@@ -1,9 +1,25 @@
-﻿namespace XN_Blazor.Data
+﻿using System;
+
+namespace XN_Blazor.Data
 {
     public enum DateOption
     {
-        Day,
         Week,
-        Month
+        Month,
+        Year
     }
+    public static class DateRangeOption
+    {
+       public static string[] GetWeekDays(DateTime dateTime)
+        {
+            string[] days = new string[7];
+
+            for(int i = 0; i < days.Length; i++)
+            {
+                days[i] = dateTime.AddDays(i).Day.ToString()+"일";
+            }
+            return days;
+        }
+    }
+
 }
